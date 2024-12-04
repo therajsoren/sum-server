d3848c7b12921cd3287ac84a80f76c7628ddc6ad
Raj Soren <7rajkumarsoren@gmail.com>
2024-12-05 00:28:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/sum", function(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    const sum = a + b;
+    res.send(sum.toString());
+});
+
+app.listen(3000);
